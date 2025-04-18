42903eb166cf4151729b4c8fab365f20c972c590
laskisami75 <laskisami75@gmail.com>
2025-04-19 00:30:21 +0300

    Compile #5 from 2025-04-19 00:30:21

--- editor/editor.js
+++ editor/editor.js
@@ -3,3 +3,18 @@ import '../ext.js'
 $('#btnReplace').onclick = e => {
   $('[role=dialog]').toggleAttribute('hidden', false)
 }
+$('#btnClose').onclick = e => {
+  $('[role=dialog]').toggleAttribute('hidden', true)
+}
+$('#btnOk').onclick = e => {
+  $('[role=dialog]').toggleAttribute('hidden', true)
+  
+  const text = $('#editor').value
+  const pattern = $('#pattern').textContent
+  const replacement = $('#replacement').textContent
+  const patternMatch = pattern.match(/^\/(.+)\/([gmsi]+)$/)
+  if (!patternMatch)
+    $('#editor').value = text.replaceAll(pattern, replacement)
+  else
+    $('#editor').value = text.replace(regex(patternMatch[2])`${patternMatch[1]}`, replacement)
+}
